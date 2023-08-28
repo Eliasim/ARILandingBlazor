68899dfd0583966d6470801595eaa35334c50f22
Eliasim Mendoza <eliasimmzsa@gmail.com>
2023-08-28 14:12:06 -0600

    Cambios hasta antes de meter cosas de correos

---
+++ ARILandingBlazor/Client/Pages/Index.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.JSInterop;
+
+namespace ARILandingBlazor.Client.Pages
+{
+    public partial class Index
+    {
+        public bool MensualPaymentType = false;
+        public string Plan { get; set; } = "Anual";
+        public string Demo { get; set; } = "";
+        public string Package { get; set; } = "";
+        public void SwitchPayment()
+        {
+            MensualPaymentType = !MensualPaymentType;
+            if (Plan == "Anual")
+            {
+                Plan = "Mensual";
+            }
+            else if (Plan == "Mensual")
+            {
+                Plan = "Anual";
+            }
+        }
+        public async void ActivateDemoButton()
+        {
+            Demo = "Solicita Demostración";
+            await JSRuntime.InvokeVoidAsync("GoContact");
+        }
+        public async void Premium()
+        {
+            Package = "PLAN PREMIUM";
+            await JSRuntime.InvokeVoidAsync("GoContact");
+        }
+        public async void Gold()
+        {
+            Package = "PLAN GOLD";
+            await JSRuntime.InvokeVoidAsync("GoContact");
+        }
+        public async void Platinum()
+        {
+            Package = "PLAN PLATINUM";
+            await JSRuntime.InvokeVoidAsync("GoContact");
+        }
+    }
+
+}
