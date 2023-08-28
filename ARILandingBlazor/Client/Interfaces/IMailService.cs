cec5d92568cb4ff931f90863b4886a7a91dde69c
Eliasim Mendoza <eliasimmzsa@gmail.com>
2023-08-28 17:49:23 -0600

    commit raro

--- ARILandingBlazor/Client/Interfaces/IMailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ARILandingBlazor.Client.Model;
-
-namespace ARILandingBlazor.Client.Interfaces
-{
-    public interface IMailService
-    {
-        Task<bool> Email(EmailBody email);
-    }
-}
